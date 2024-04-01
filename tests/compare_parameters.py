5060233ea9e09035364cf5a0cf823fee1893c0e0
kazukingh01 <kazukingh01@gmail.com>
2024-04-01 21:42:03 +0900

    modify

--- tests/compare_parameters.py
+++ tests/compare_parameters.py
@@ -39,27 +39,46 @@ if __name__ == "__main__":
         "min_data_in_bin": 1,
         "min_child_weight": 1e-3,
     }
+    loss_func = {
+        "xgb": "multi:softmax",
+        "lgb": "multiclass",
+    }
+    loss_func_eval = {
+        "xgb": "mlogloss",
+        "lgb": "multiclass",
+    }
     for max_bin in [32, 64, 128, 256]:
         for num_leaves in [50, 100, 200]:
             for max_depth in [-1, 5, 10]:
                 for colsample_bytree in [0.1, 0.3, 0.6, 1.0]:
-                    for random_seed in range(20):
-                        param = (params_fix | {"max_bin": max_bin, "num_leaves": num_leaves, "max_depth": max_depth, "colsample_bytree": colsample_bytree, "random_seed": random_seed})
-                        model = KkGBDT(n_class, mode="xgb", **param)
-                        model.fit(
-                            train_x, train_y, loss_func="multi:softmax", num_iterations=300,
-                            x_valid=valid_x, y_valid=valid_y, loss_func_eval=["mlogloss"], sample_weight="balanced"
-                        )
-                        se = pd.Series(param)
-                        se["time_xgb"] = model.time_train
-                        se["eval_xgb"] = log_loss(valid_y, model.predict(valid_x, is_softmax=True))
-                        model = KkGBDT(n_class, mode="lgb", **param)
-                        model.fit(
-                            train_x, train_y, loss_func="multiclass", num_iterations=300,
-                            x_valid=valid_x, y_valid=valid_y, loss_func_eval=["multiclass"], sample_weight="balanced"
-                        )
-                        se["time_lgb"] = model.time_train
-                        se["eval_lgb"] = log_loss(valid_y, model.predict(valid_x, is_softmax=False))
-                        df_eval.append(se)
+                    for num_grad_quant_bins in [0, 4, 8, 16]:
+                        for random_seed in range(20):
+                            for mode in ["xgb", "lgb"]:
+                                param = (params_fix | {"max_bin": max_bin, "num_leaves": num_leaves, "max_depth": max_depth, "colsample_bytree": colsample_bytree, "mode": mode, "random_seed": random_seed})
+                                if num_grad_quant_bins > 0:
+                                    param["use_quantized_grad"] = True
+                                    param["num_grad_quant_bins"] = num_grad_quant_bins
+                                else:
+                                    param["use_quantized_grad"] = False
+                                if param["use_quantized_grad"] and mode == "xgb":
+                                    continue
+                                model = KkGBDT(n_class, **param)
+                                model.fit(
+                                    train_x, train_y, loss_func=loss_func[mode], num_iterations=300,
+                                    x_valid=valid_x, y_valid=valid_y, loss_func_eval=loss_func_eval[mode], sample_weight="balanced"
+                                )
+                                se = pd.Series(param)
+                                se["time"] = model.time_train
+                                se["eval"] = log_loss(valid_y, model.predict(valid_x, is_softmax=True))
+                                model = KkGBDT(n_class, **param)
+                                model.fit(
+                                    train_x, train_y, loss_func=loss_func[mode], num_iterations=300,
+                                    x_valid=valid_x, y_valid=valid_y, loss_func_eval=loss_func_eval[mode], sample_weight="balanced",
+                                    early_stopping_rounds=30, early_stopping_name=0, 
+                                )
+                                se = pd.Series(param)
+                                se["time_erstp"] = model.time_train
+                                se["eval_erstp"] = log_loss(valid_y, model.predict(valid_x, is_softmax=True))
+                                df_eval.append(se)
     df_eval = pd.concat(df_eval, axis=1, ignore_index=True).T
     df_eval.to_pickle("df_eval.pickle")
