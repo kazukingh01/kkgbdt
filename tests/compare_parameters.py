c9b6e60b85dd50be58b3de6cb6974b551c6f061a
kazukingh01 <kazukingh01@gmail.com>
2024-04-02 10:37:09 +0900

    modify

--- tests/compare_parameters.py
+++ tests/compare_parameters.py
@@ -39,6 +39,7 @@ if __name__ == "__main__":
         "min_split_gain": 0.0,
         "min_data_in_bin": 1,
         "min_child_weight": 1e-3,
+        "colsample_bytree": 0.5
     }
     loss_func = {
         "xgb": "multi:softmax",
@@ -54,27 +55,26 @@ if __name__ == "__main__":
     }
     for max_bin in [32, 64, 128, 256]:
         for num_leaves in [50, 100, 200]:
-            for max_depth in [-1, 5, 10]:
-                for colsample_bytree in [0.1, 0.3, 0.6, 1.0]:
-                    for num_grad_quant_bins in [0, 4, 8, 16]:
-                        for random_seed in range(20):
-                            for mode in ["xgb", "lgb"]:
-                                param = (params_fix | {"max_bin": max_bin, "num_leaves": num_leaves, "max_depth": max_depth, "colsample_bytree": colsample_bytree, "mode": mode, "random_seed": random_seed})
-                                if num_grad_quant_bins > 0:
-                                    param["use_quantized_grad"] = True
-                                    param["num_grad_quant_bins"] = num_grad_quant_bins
-                                else:
-                                    param["use_quantized_grad"] = False
-                                if param["use_quantized_grad"] and mode == "xgb":
-                                    continue
-                                model = KkGBDT(n_class, **param)
-                                model.fit(
-                                    train_x, train_y, loss_func=loss_func[mode], num_iterations=args.iter,
-                                    x_valid=valid_x, y_valid=valid_y, loss_func_eval=loss_func_eval[mode], sample_weight="balanced"
-                                )
-                                se = pd.Series(param)
-                                se["time"] = model.time_train
-                                se["eval"] = log_loss(valid_y, model.predict(valid_x, is_softmax=is_softmax[mode], iteration_at=model.best_iteration))
-                                df_eval.append(se)
+            for max_depth in [-1, 6, 12]:
+                for num_grad_quant_bins in [0, 4, 8, 16]:
+                    for random_seed in range(10):
+                        for mode in ["xgb", "lgb"]:
+                            param = (params_fix | {"max_bin": max_bin, "num_leaves": num_leaves, "max_depth": max_depth, "colsample_bytree": colsample_bytree, "mode": mode, "random_seed": random_seed})
+                            if num_grad_quant_bins > 0:
+                                param["use_quantized_grad"] = True
+                                param["num_grad_quant_bins"] = num_grad_quant_bins
+                            else:
+                                param["use_quantized_grad"] = False
+                            if param["use_quantized_grad"] and mode == "xgb":
+                                continue
+                            model = KkGBDT(n_class, **param)
+                            model.fit(
+                                train_x, train_y, loss_func=loss_func[mode], num_iterations=args.iter,
+                                x_valid=valid_x, y_valid=valid_y, loss_func_eval=loss_func_eval[mode], sample_weight="balanced"
+                            )
+                            se = pd.Series(param)
+                            se["time"] = model.time_train
+                            se["eval"] = log_loss(valid_y, model.predict(valid_x, is_softmax=is_softmax[mode], iteration_at=model.best_iteration))
+                            df_eval.append(se)
     df_eval = pd.concat(df_eval, axis=1, ignore_index=True).T
     df_eval.to_pickle("df_eval.pickle")
