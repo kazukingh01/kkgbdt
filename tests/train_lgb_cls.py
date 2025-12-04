7e3f0d520d676c4015fc22ac424b4474d3ecd521
kazukingh01 <you@example.com>
2025-12-04 23:10:31 +0900

    modify

--- tests/train_lgb_cls.py
+++ tests/train_lgb_cls.py
@@ -2,7 +2,7 @@ import json
 import lightgbm as lgb
 import numpy as np
 import pandas as pd
-from sklearn.datasets import fetch_covtype 
+from kktestdata import DatasetRegistry
 from kkgbdt.model import KkGBDT
 from kkgbdt.loss import CategoricalCrossEntropyLoss, CrossEntropyLoss, Accuracy, FocalLoss, \
     CrossEntropyLossArgmax, BinaryCrossEntropyLoss, CrossEntropyNDCGLoss, LogitMarginL1Loss
@@ -15,12 +15,12 @@ LOGGER = set_logger(__name__)
 
 
 if __name__ == "__main__":
-    data = fetch_covtype()
-    train_x = data["data"  ][:-data["target"].shape[0]//5 ]
-    train_y = data["target"][:-data["target"].shape[0]//5 ] - 1
-    valid_x = data["data"  ][ -data["target"].shape[0]//5:]
-    valid_y = data["target"][ -data["target"].shape[0]//5:] - 1
-    n_class = np.unique(train_y).shape[0]
+    reg     = DatasetRegistry()
+    dataset = reg.create("gas-drift")
+    train_x, train_y, valid_x, valid_y, test_x, test_y = dataset.load_data(
+        format="numpy", split_type="valid", test_size=0.3, valid_size=0.2
+    )
+    n_class = dataset.metadata.n_classes
     n_iter  = 500
     lr      = 0.05
     max_bin = 64
@@ -30,7 +30,6 @@ if __name__ == "__main__":
     LOGGER.info(f"\n{pd.DataFrame(train_y).groupby(0).size()}")
     LOGGER.info("data valid", color=["BOLD", "CYAN"])
     LOGGER.info(f"\n{pd.DataFrame(valid_y).groupby(0).size()}")
-
     LOGGER.info("Test training stop", color=["BOLD", "UNDERLINE", "GREEN"])
     model   = KkGBDT(n_class, mode="lgb", learning_rate=lr, max_bin=max_bin, max_depth=ndepth)
     model.fit(
@@ -49,9 +48,9 @@ if __name__ == "__main__":
         x_valid=valid_x, y_valid=valid_y, loss_func_eval=["multiclass", Accuracy(top_k=2)], sample_weight="balanced",
         early_stopping_rounds=20, early_stopping_name=0,
     )
-    ndf_pred = model.predict(valid_x, iteration_at=model.best_iteration)
-    valeval["multiclass_log"] = log_loss(valid_y, ndf_pred)
-    valeval["multiclass_acc"] = Accuracy(top_k=2)(ndf_pred, valid_y)
+    ndf_pred = model.predict(test_x, iteration_at=model.best_iteration)
+    valeval["multiclass_log"] = log_loss(test_y, ndf_pred)
+    valeval["multiclass_acc"] = Accuracy(top_k=2)(ndf_pred, test_y)
 
     LOGGER.info('public loss with "use_quantized_grad"', color=["BOLD", "UNDERLINE", "GREEN"])
     try:
@@ -86,9 +85,9 @@ if __name__ == "__main__":
         x_valid=valid_x, y_valid=valid_y, loss_func_eval=["__copy__", CategoricalCrossEntropyLoss(n_class), Accuracy(top_k=2)],
         early_stopping_rounds=20, early_stopping_name=0, sample_weight="balanced",
     )
-    ndf_pred = model.predict(valid_x)
-    valeval["CategoryCE_log"] = log_loss(valid_y, ndf_pred)
-    valeval["CategoryCE_acc"] = Accuracy(top_k=2)(ndf_pred, valid_y)
+    ndf_pred = model.predict(test_x)
+    valeval["CategoryCE_log"] = log_loss(test_y, ndf_pred)
+    valeval["CategoryCE_acc"] = Accuracy(top_k=2)(ndf_pred, test_y)
 
     LOGGER.info("custom loss FocalLoss", color=["BOLD", "UNDERLINE", "GREEN"])
     model = KkGBDT(n_class, mode="lgb", learning_rate=lr, max_bin=max_bin, max_depth=ndepth)
@@ -97,9 +96,9 @@ if __name__ == "__main__":
         x_valid=valid_x, y_valid=valid_y, loss_func_eval=["__copy__", CategoricalCrossEntropyLoss(n_class), Accuracy(top_k=2)],
         early_stopping_rounds=20, early_stopping_name=0, sample_weight="balanced",
     )
-    ndf_pred = model.predict(valid_x)
-    valeval["FocalLoss_log"] = log_loss(valid_y, ndf_pred)
-    valeval["FocalLoss_acc"] = Accuracy(top_k=2)(ndf_pred, valid_y)
+    ndf_pred = model.predict(test_x)
+    valeval["FocalLoss_log"] = log_loss(test_y, ndf_pred)
+    valeval["FocalLoss_acc"] = Accuracy(top_k=2)(ndf_pred, test_y)
 
     LOGGER.info("custom loss LogitMarginL1Loss", color=["BOLD", "UNDERLINE", "GREEN"])
     model = KkGBDT(n_class, mode="lgb", learning_rate=lr, max_bin=max_bin, max_depth=ndepth)
@@ -108,9 +107,9 @@ if __name__ == "__main__":
         x_valid=valid_x, y_valid=valid_y, loss_func_eval=["__copy__", CategoricalCrossEntropyLoss(n_class), Accuracy(top_k=2)],
         early_stopping_rounds=20, early_stopping_name=0, sample_weight="balanced",
     )
-    ndf_pred = model.predict(valid_x)
-    valeval["LogitMarginL1Loss_log"] = log_loss(valid_y, ndf_pred)
-    valeval["LogitMarginL1Loss_acc"] = Accuracy(top_k=2)(ndf_pred, valid_y)
+    ndf_pred = model.predict(test_x)
+    valeval["LogitMarginL1Loss_log"] = log_loss(test_y, ndf_pred)
+    valeval["LogitMarginL1Loss_acc"] = Accuracy(top_k=2)(ndf_pred, test_y)
 
     LOGGER.info("custom loss CrossEntropyLoss", color=["BOLD", "UNDERLINE", "GREEN"])
     model      = KkGBDT(n_class, mode="lgb", learning_rate=lr, max_bin=max_bin, max_depth=ndepth)
@@ -123,9 +122,9 @@ if __name__ == "__main__":
         x_valid=valid_x, y_valid=valid_y_ce, loss_func_eval=["__copy__", CrossEntropyLossArgmax(n_class), Accuracy(top_k=2)],
         early_stopping_rounds=20, early_stopping_name=0
     )
-    ndf_pred = model.predict(valid_x)
-    valeval["CrossEntropyLoss_log"] = log_loss(valid_y, ndf_pred)
-    valeval["CrossEntropyLoss_acc"] = Accuracy(top_k=2)(ndf_pred, valid_y)
+    ndf_pred = model.predict(test_x)
+    valeval["CrossEntropyLoss_log"] = log_loss(test_y, ndf_pred)
+    valeval["CrossEntropyLoss_acc"] = Accuracy(top_k=2)(ndf_pred, test_y)
 
     LOGGER.info("custom loss BinaryCrossEntropyLoss", color=["BOLD", "UNDERLINE", "GREEN"])
     list_pred = []
@@ -136,11 +135,11 @@ if __name__ == "__main__":
             x_valid=valid_x, y_valid=_valid_y, loss_func_eval=["__copy__", Accuracy(top_k=1)],
             early_stopping_rounds=20, early_stopping_name=0, sample_weight="balanced",
         )
-        list_pred.append(model.predict(valid_x))
+        list_pred.append(model.predict(test_x))
     ndf = np.stack(list_pred).T
     ndf_pred = ndf / ndf.sum(axis=-1).reshape(-1, 1)
-    valeval["BinaryCrossEntropyLoss_log"] = log_loss(valid_y, ndf_pred)
-    valeval["BinaryCrossEntropyLoss_acc"] = Accuracy(top_k=2)(ndf_pred, valid_y)
+    valeval["BinaryCrossEntropyLoss_log"] = log_loss(test_y, ndf_pred)
+    valeval["BinaryCrossEntropyLoss_acc"] = Accuracy(top_k=2)(ndf_pred, test_y)
 
     LOGGER.info("custom loss CrossEntropyNDCGLoss", color=["BOLD", "UNDERLINE", "GREEN"])
     train_y_ndcg = np.ones((train_y.shape[0], n_class), dtype=int)
@@ -153,8 +152,8 @@ if __name__ == "__main__":
         x_valid=valid_x, y_valid=valid_y_ndcg, loss_func_eval="__copy__",
         early_stopping_rounds=20, early_stopping_name=0, sample_weight=None,
     )
-    ndf_pred = model.predict(valid_x)
-    valeval["CrossEntropyNDCGLoss_log"] = log_loss(valid_y, ndf_pred)
-    valeval["CrossEntropyNDCGLoss_acc"] = Accuracy(top_k=2)(ndf_pred, valid_y)
+    ndf_pred = model.predict(test_x)
+    valeval["CrossEntropyNDCGLoss_log"] = log_loss(test_y, ndf_pred)
+    valeval["CrossEntropyNDCGLoss_acc"] = Accuracy(top_k=2)(ndf_pred, test_y)
 
     LOGGER.info(f"{json.dumps({x:float(y) for x, y in valeval.items()}, indent=2)}")
--- tests/train_lgb_rank.py
+++ tests/train_lgb_rank.py
@@ -1,7 +1,7 @@
-import zipfile
 import pandas as pd 
 from kklogger import set_logger
 import numpy as np
+from kktestdata import DatasetRegistry
 from kkgbdt.model import KkGBDT
 from kkgbdt.functions import evaluate_ndcg
 
@@ -10,35 +10,23 @@ LOGGER = set_logger(__name__)
 
 
 if __name__ == '__main__':
-    # data load
-    with zipfile.ZipFile('./boatrace_course.zip', 'r') as z:
-        with z.open('boatrace_course.csv') as f:
-            df = pd.read_csv(f, index_col=False)
-    colsoth = ["race_id", "course", "move_real"]
-    df      = df.iloc[:, np.where(~(df.columns.isin(colsoth)))[0].tolist() + np.where((df.columns.isin(colsoth)))[0].tolist()]
-    df["answer"]  = df["course"].map({1:6, 2:5, 3:4, 4:3, 5:2, 6:1}) # Good course is given good point
-    df["race_id"] = df["race_id"].astype(str)
-    ndf_bool = (df["race_id"] >= "202001010000").to_numpy(dtype=bool)
-    df_valid = df.loc[ndf_bool].copy()
-    df_train = df.loc[df.index.isin(df_valid.index) == False].copy()
-    train_x  = df_train.iloc[:, :-(len(colsoth) + 1)].to_numpy(dtype=np.float32)
-    train_y  = df_train["answer" ].to_numpy(dtype=object)
-    train_g  = df_train["race_id"].to_numpy(dtype=object)
-    valid_x  = df_valid.iloc[:, :-(len(colsoth) + 1)].to_numpy(dtype=np.float32)
-    valid_y  = df_valid["answer" ].to_numpy(dtype=object)
-    valid_g  = df_valid["race_id"].to_numpy(dtype=object)
+    reg     = DatasetRegistry()
+    dataset = reg.create("boatrace_2020_2021")
+    train_x, train_y, valid_x, valid_y, test_x, test_y = dataset.load_data(
+        format="numpy", split_type="valid", test_size=0.3, valid_size=0.2
+    )
     # training
     model = KkGBDT(1, mode="lgb", learning_rate=0.1, max_bin=64, max_depth=-1, eval_at=[3, 6])
     model.fit(
-        train_x, train_y, loss_func="lambdarank", num_iterations=200,
+        train_x[:, 1:], train_y, loss_func="lambdarank", num_iterations=200,
         x_valid=valid_x, y_valid=valid_y, loss_func_eval="lambdarank",
-        early_stopping_rounds=20, early_stopping_name=1, group_train=train_g, group_valid=valid_g,
-        categorical_features=np.where(df_train.columns.isin(["player_no", "number", "exhibition_course", "jcd"]))[0].tolist(),
+        early_stopping_rounds=20, early_stopping_name=1, group_train=train_x[:, 0], group_valid=valid_x[:, 0],
+        categorical_features=(
+            np.where(np.isin(np.array(dataset.metadata.columns_feature), ["player_no", "number", "exhibition_course"]))[0] - 1
+        ).tolist(),
     )
     # evaluation
-    ndf_pred = model.predict(valid_x)
-    df_valid["pred"] = -ndf_pred
-    df_valid["pred"] = df_valid.groupby("race_id")["pred"].rank().astype(int)
-    pred = df_valid.groupby("race_id")[["exhibition_course", "pred", "course"]].apply(lambda x: evaluate_ndcg(x["pred"             ].to_numpy(), x["course"].to_numpy()))
-    base = df_valid.groupby("race_id")[["exhibition_course", "pred", "course"]].apply(lambda x: evaluate_ndcg(x["exhibition_course"].to_numpy(), x["course"].to_numpy()))
-    LOGGER.info(f"pred: {pred.mean()}, base: {base.mean()}", color=["GREEN", "BOLD"])
+    ndf_pred = model.predict(test_x[:, 1:])
+    benchmark = evaluate_ndcg(ndf_pred,     test_y, is_point_to_rank=True,  k=6, idx_groups=test_x[:, 0])
+    pred      = evaluate_ndcg(test_x[:, 1], test_y, is_point_to_rank=False, k=6, idx_groups=test_x[:, 0])
+    LOGGER.info(f"pred: {pred.mean()}, base: {benchmark.mean()}", color=["GREEN", "BOLD"])
